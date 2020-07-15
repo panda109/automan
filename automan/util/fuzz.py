9e5077f2bb571e501633c93a4d840283c0b06bc1
chliuhelmut <clowndoubting@hotmail.com>
2020-07-15 14:58:37 +0800

    Create fuzz.py

---
+++ automan/util/fuzz.py
@@ -0,0 +1,31 @@
+'''
+Created on 2020/4/16
+
+@author: helmut liu
+
+'''
+from saker.fuzzers.fuzzer import Fuzzer
+from saker.brute.brute  import Brute
+from saker.api.dnsdumper import DNSdumpster
+from saker.brute.basicAuth import BasicAuth
+from saker.handler.headerHandler import HeaderHandler
+
+
+
+
+
+class fuzz(object):
+    def __init__(self):
+        '''
+        '''
+        
+    def web_scan(self):
+    
+        '''
+        '''
+    def scan_api(self):
+       '''
+       '''
+    def input_mutator(self):
+       '''
+       '''    

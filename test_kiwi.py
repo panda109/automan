c0e29a114ad7b0ecbd02f9a70c3f05e391487a05
automan <automan@DESKTOP-6UHVU9P>
2021-01-12 13:36:43 +0800

    update

--- test_kiwi.py
+++ test_kiwi.py
@@ -4,5 +4,13 @@ Created on Aug 21, 2020
 @author: tim
 '''
 
+'''
+https://{API_SEVER}/api/v1/subset/{subset}/projects/{projectName}/approval/{buildNumber}
+{
+  "qaComments": the_comments,    
+  "qaApproval": "approved"             // {"approved" | "rejected"} 
+}
+'''
+
 if __name__ == '__main__':
     pass

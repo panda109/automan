06ae43053f38babe801bc133d1166b5e07d8b11f
automan <automan@DESKTOP-6UHVU9P>
2021-01-14 11:21:49 +0800

    test

--- Update_result.py
+++ Update_result.py
@@ -17,22 +17,25 @@ from junitparser import JUnitXml
 if __name__ == '__main__':
     argv = sys.argv[1:]
     project = argv[0]
-    buildnumber = argv[2]
+    version = argv[2]
     subset = argv[1]
     logfile = argv[3]
     xml = JUnitXml.fromfile(logfile)
     time.sleep(1)
-    res = "approved"
+    res = '{"qaComments": "QA_auto_test_result","qaApproval" : "approved"}'
     for suite in xml:
         if (str(suite).find('pass') == -1):
-            res = "rejected"
-    qa_std = "https://frs.nextdrive.io/api/v1/subset/"+subset+"/projects/"+project+"/approval/"+buildnumber
-    #qa_std = "https://frs.nextdrive.io/api/v1/subset/ndos2/projects/Cube-Test/approval/"+buildnumber
+            res = '{"qaComments": "QA_auto_test_result","qaApproval" : "rejected"}'
+    #qa_std = "https://frs-api.nextdrive.io/api/v1/subset/"+subset+"/projects/"+project+"/approval/"+version
+    #qa_std = "https://radiant-wildwood-74214.herokuapp.com/api/v1/subset/ndos2/projects/Cube-Test/approval/4.0.51007"
+    qa_std = "https://frs-api.nextdrive.io/api/v1/subset/ndos2/projects/Cube-Test/approval/4.0.51008"
+    #qa_std = "https://frs-api.nextdrive.io/api/v1/subset/ndos2/projects/Cube-Test/approval/4.0.51005"
     print(qa_std)
-    json_data = {
-            "qaComments": "QA auto test result",    
-            "qaApproval": res
-            }
+    json_data = res
+    header = {'content-type': 'application/json',
+              'authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTYsImRhdGEiOnsidXNlcm5hbWUiOiJqZW5raW5zIiwiZW1haWwiOiJqZW5raW5zQG5leHRkcml2ZS5pbyIsInJvbGUiOjV9LCJpYXQiOjE1OTk2MTQ4ODEsImF1ZCI6ImRiNWEzODk0LTY5ZTgtNDZiYy1iMDZjLWMzZWY2YTNjZGEyMCIsImlzcyI6Im5leHRkcml2ZS5pbyIsImp0aSI6ImFmZDZlNzkxLTcwNGMtNGViNy1iMDcxLWI5NzBhZTRjZDgzNiJ9.blei-5XAwzMB6KmlfPqPmsstWhExAleaUw9P3kUWmuDW8WJhalPD87W9p6r95PRgjIShU82jtGMugDLmU0H7pcTPMz0nZVLaUwoKJD5KFC55QOZPCjkEv4tvidgurilD-EEQ3EPotGu18p-8v1gu5s_phW7Xj3MFBrR0kv05FeiXvMsBkHdNIDUCcVzDvZsaVnHjM3eSxJB5kUmYYpz9w2DjrPvMqb7nZBAxW_eWn5CfKunBfTKYJHwcMljLcO2FWzZ6o-IPinryTSvtVzdwXWXkIBHtMkL46UkVVjykiLQ_3sxUolfkSLoUbgq0yO8RZoVZVxo1pIxYBcuPlJ3E6w' 
+              }
     #{"approved" | "rejected"} 
-    r = requests.post(qa_std, data = json_data )
-    print(r)
+    print(json_data)
+    r = requests.put(qa_std, data = json_data, headers = header )
+    print(str(r.content))

4de09661bba461d02d82be9388d6f1972a0664de
shawn <shawn@192.168.7.79>
2021-03-05 18:14:29 +0800

    fix bug

--- download_fw.py
+++ download_fw.py
@@ -12,11 +12,17 @@ strURLTemplate = 'https://download.nextdrive.io/%s/%s/%s'
 if __name__ == '__main__':
     listArgv = sys.argv[1:]
     strProject = listArgv[0]
-    strVersion = listArgv[1]
+    trVersion = listArgv[1]
     #strSubset = listArgv[1]
     strTargetPath = listArgv[2]
+    #strProject = 'cube-plus'
+    #strVersion = '4.0.16023'
+    #strTargetPath = '/Volumes/build/ota-test-data'
     
     strTargetPath = os.path.join(strTargetPath,strProject)
+    if not os.path.exists(strTargetPath):
+        os.mkdir(strTargetPath)
+
     strTargetPath = os.path.join(strTargetPath,strVersion)
     if not os.path.exists(strTargetPath):
         os.mkdir(strTargetPath)
